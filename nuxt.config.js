c81fcbaa451b6449980d45a9dc0c538526733297
Evgeniy <sonicbhf@gmail.com>
2021-05-14 22:44:49 +0300

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  server: {
+    host: '0.0.0.0',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - app-rickandmorty',
