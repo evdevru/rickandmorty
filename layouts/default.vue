b502a73232424efb2b6a97cae4db8d584378958a
Evgeniy <sonicbhf@gmail.com>
2021-05-14 22:34:02 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer dense v-model="drawer" clipped fixed app>
+      <v-list>
+        <v-list-item :to="{ name: 'characters' }" router exact>
+          <v-list-item-action>
+            <v-icon>mdi-account-multiple</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="'Characters'" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-map-marker-multiple</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="'Locations'" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-account-multiple</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="'Episodes'" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <template #append>
+        <div class="d-flex justify-space-around pt-3 pb-3">
+          <v-tooltip top>
+            <template #activator="{ on, attrs }">
+              <v-icon v-bind="attrs" v-on="on">mdi-facebook</v-icon>
+            </template>
+            <span>Facebook</span>
+          </v-tooltip>
+          <v-tooltip top>
+            <template #activator="{ on, attrs }">
+              <v-icon v-bind="attrs" v-on="on">mdi-instagram</v-icon>
+            </template>
+            <span>Instagram</span>
+          </v-tooltip>
+          <v-tooltip top>
+            <template #activator="{ on, attrs }">
+              <v-icon v-bind="attrs" v-on="on">mdi-reddit</v-icon>
+            </template>
+            <span>Reddit</span>
+          </v-tooltip>
+        </div>
+      </template>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="true" :fixed="false" app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-avatar color="accent" size="46"></v-avatar>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      title: 'Rick and Morty',
+    }
+  },
+}
+</script>
---
+++ pages/characters.vue
@@ -0,0 +1,210 @@
+<template>
+  <section>
+    <h1 class="d-block mb-6">Characters</h1>
+    <v-row class="sticky-top grey darken-3">
+      <v-col cols="12" md="4">
+        <v-text-field
+          v-model="filter.name"
+          label="Name"
+          outlined
+          clearable
+          dense
+          hide-details
+          @input="resetPage"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-select
+          v-model="filter.status"
+          :items="statuses"
+          label="Status"
+          dense
+          outlined
+          clearable
+          hide-details
+          @change="resetPage"
+        ></v-select>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-select
+          v-model="filter.gender"
+          :items="genders"
+          label="Gender"
+          dense
+          outlined
+          clearable
+          hide-details
+          @change="resetPage"
+        ></v-select>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        v-for="character in characters"
+        :key="character.id"
+        cols="6"
+        md="4"
+        lg="3"
+        xl="2"
+      >
+        <v-lazy>
+          <v-card>
+            <v-img :aspect-ratio="1" :src="character.image"></v-img>
+
+            <v-card-title class="text-truncate">{{
+              character.name
+            }}</v-card-title>
+
+            <v-card-subtitle> {{ character.species }} </v-card-subtitle>
+          </v-card>
+        </v-lazy>
+      </v-col>
+      <template v-if="loading"
+        ><v-col
+          v-for="i in 10"
+          :key="`card-skeleton-${i}`"
+          cols="6"
+          md="4"
+          lg="3"
+          xl="2"
+        >
+          <v-skeleton-loader
+            class="mx-auto"
+            max-width="300"
+            type="card"
+          ></v-skeleton-loader> </v-col
+      ></template>
+    </v-row>
+    <div v-if="info.pages <= filter.page" class="text-center my-4">
+      No more characters
+    </div>
+    <div v-if="error.length" class="text-center my-4">{{ error }}</div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      characters: [],
+      maxPages: 2,
+      loading: true,
+      error: '',
+      timeout: {},
+      info: {},
+      filter: {
+        page: 1,
+        name: '',
+        gender: '',
+        status: '',
+      },
+      genders: [
+        {
+          text: 'Female',
+          value: 'Female',
+        },
+        {
+          text: 'Male',
+          value: 'Male',
+        },
+        {
+          text: 'Genderless',
+          value: 'Genderless',
+        },
+        {
+          text: 'Unknown',
+          value: 'unknown',
+        },
+      ],
+      statuses: [
+        {
+          text: 'Alive',
+          value: 'Alive',
+        },
+        {
+          text: 'Dead',
+          value: 'Dead',
+        },
+        {
+          text: 'unknown',
+          value: 'unknown',
+        },
+      ],
+    }
+  },
+  fetch() {
+    this.getCharacters()
+  },
+  watch: {
+    filter: {
+      handler(val, oldVal) {
+        if (this.filter.page <= this.info.pages) this.getCharacters()
+      },
+      deep: true,
+    },
+  },
+  mounted() {
+    this.scrollHandler()
+  },
+  methods: {
+    async getCharacters() {
+      this.loading = true
+      this.error = ''
+      try {
+        const filter = Object.fromEntries(
+          Object.entries(this.filter).filter(([_, v]) => v != null)
+        )
+        const query = new URLSearchParams(filter).toString()
+        const { data } = await this.$axios.get(
+          `https://rickandmortyapi.com/api/character/?${query}`
+        )
+        this.info = data.info
+
+        if (this.filter.page === 1) {
+          this.characters = data.results
+        } else {
+          this.characters.push(...data.results)
+        }
+      } catch (e) {
+        this.characters = []
+        this.info = {}
+        this.error = e.response.data.error
+      }
+      this.loading = false
+    },
+    scrollHandler(e) {
+      window.onscroll = () => {
+        const bottomOfWindow =
+          Math.max(
+            window.pageYOffset,
+            document.documentElement.scrollTop,
+            document.body.scrollTop
+          ) +
+            window.innerHeight >
+          document.documentElement.offsetHeight - 300
+
+        if (bottomOfWindow) {
+          clearTimeout(this.timeout)
+          this.timeout = setTimeout(() => {
+            this.filter.page++
+          }, 300)
+        }
+      }
+    },
+    resetPage() {
+      this.info.pages = 1
+      this.filter.page = 1
+    },
+  },
+}
+</script>
+
+<style>
+@media (min-width: 960px) {
+  .sticky-top {
+    position: sticky;
+    top: 64px;
+    z-index: 10;
+  }
+}
+</style>
